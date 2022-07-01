872f7f8deb06f1cf1c5a917e9774afae2bd3ab72
Paulo Salvatore <salvatoregames@gmail.com>
2022-07-01 09:56:08 -0300

    Desafio resolvido

--- app/src/main/java/com/oceanbrasil/ocean_android_introducao_30_06_2022/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_introducao_30_06_2022/MainActivity.kt
@@ -2,6 +2,7 @@ package com.oceanbrasil.ocean_android_introducao_30_06_2022
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,13 +14,27 @@ class MainActivity : AppCompatActivity() {
         // Buscar o botão pelo ID
         val btEnviar = findViewById<Button>(R.id.btEnviar)
         val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        val etNome = findViewById<EditText>(R.id.etNome)
 
         btEnviar.setOnClickListener {
             // Dentro deste bloco, estamos dentro do clickListener
             // Portanto, o código que está aqui será executado
             // quando o botão for clicado (clique = toque no celular)
 
-            tvResultado.text = "Paulo Salvatore"
+            /*
+            Pegar o que está escrito no etNome
+            Checar se o que está escrito é vazio ou não
+            A partir disso, se estiver vazio, preciso mostrar uma mensagem de erro
+            Caso contrário, atualizo o texto no TextView com o que foi digitado no EditText
+            */
+
+            val nomeDigitado = etNome.text
+
+            if (nomeDigitado.isBlank()) {
+                etNome.error = "Digite um nome válido!"
+            } else {
+                tvResultado.text = nomeDigitado
+            }
         }
     }
 }
